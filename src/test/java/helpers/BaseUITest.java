e18f0310178270d29dbcdfc35edff60247ee1de3
Approximax <vlad_pw_v@list.ru>
2022-07-27 17:41:14 +0300

    Заготовка

---
+++ src/test/java/helpers/BaseUITest.java
@@ -0,0 +1,13 @@
+package helpers;
+
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+
+public class BaseUITest {
+
+    protected WebDriver driver;
+    @Before
+    public void SetUp(){
+        driver = WebDriver
+    }
+}
